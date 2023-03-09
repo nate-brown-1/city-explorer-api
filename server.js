ee4e68512edd4ae6e7f21d156f6b278155adb9e2
Nate Brown <nathan.d.brown@gmail.com>
2023-03-08 23:52:23 -0800

    commit for lab8 submit

--- server.js
+++ server.js
@@ -1,32 +1,45 @@
 'use strict';
 
+
+// express (server package)
 const express = require('express');
 
+//  environmental variable package / for API tokens
 require('dotenv').config();
 
+// weather data file, not from API
 let data = require('./data/weather.json');
 
+// cross origin resource sharing = allows JavaScript clients
 const cors = require('cors');
 
+// assign variable to express
 const app = express();
 
+// start cors
 app.use(cors());
 
+// get port number from .env / using 3001
+// when express is started
+// if .env is correct, will show as "Listening on 3001"
+// if .env is broken/missing, will show as "Listening on 3002"
 const PORT = process.env.PORT || 3002;
-
 
 // test the server
 app.get('/', (request, response) => {
   response.send('Server is working!');
 });
 
-// request format: http://localhost:3002/city?cityName=Seattle
+// request format: http://localhost:3001/city?cityName=Seattle
 app.get('/city', (request, response, next) => {
   try {
     let cityRequested = request.query.cityName;
-    let cityObject = data.find(city => city.city_name === cityRequested);
-    let selectedCity = new City(cityObject);
-    response.send(selectedCity);
+    let cityResults = data.find(city => city.city_name === cityRequested);
+    let city = data.map(city => new City(city));
+    console.log(city);
+    // let forecast = cityObject.data.map(forecast => new Forecast(forecast));
+    // console.log(forecast);
+    response.send(city);
   } catch (error) {
     next(error);
   }
@@ -39,12 +52,17 @@ app.get('*', (request, response) => {
 class City {
   constructor(CityObject) {
     this.city_name = CityObject.city_name;
+    this.lat = CityObject.lat;
+    this.lon = CityObject.lon;
   }
 }
 
-// class Forecast {
-//   constructor
-// }
+class Forecast {
+  constructor(forecast) {
+    this.date = forecast.data.date;
+    this.description = forecast.data.description
+  }
+}
 
 app.use((error, request, response, next) => {
   response.status(500).send(error.message);
